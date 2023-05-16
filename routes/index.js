3833ed6e0bbaed20cb6d11e0b75f76911b7b0c38
Andiko Yoga <90024747+DyArdh@users.noreply.github.com>
2023-05-16 20:01:14 +0700

    feat: add home routing

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,18 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const user = require('../controllers/user');
+const user = require("../controllers/user");
 
-const middlewares = require('../utils/middlewares');
+const middlewares = require("../utils/middlewares");
 
-router.post('/auth/register', user.register);
-router.post('/auth/login', user.login);
-router.get('/auth/whoami', middlewares.auth, user.whoami);
+router.get("/", (req, res) => {
+  return res.status(200).json({
+    status: true,
+    message: "Welcome to API HomePage",
+    data: null,
+  });
+});
+router.post("/auth/register", user.register);
+router.post("/auth/login", user.login);
+router.get("/auth/whoami", middlewares.auth, user.whoami);
 
 module.exports = router;
